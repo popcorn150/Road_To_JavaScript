5c0e420a42973c5d9e9ae43d9f8ace1b5875d31f
popcorn150 <hungrymanchannel@gmail.com>
2024-02-11 02:09:53 +0100

    still working on conditional statements

---
+++ Conditional Statements/logical_operators.js
@@ -0,0 +1,8 @@
+let mood = 'sleepy';
+let tirednessLevel = 10;
+
+if (mood === mood && tirednessLevel >= 8) {
+    console.log('Time to sleep')
+} else {
+ console.log('Not bed time yet')   
+}
---
+++ Conditional Statements/truthy_falsy_assignment.js
@@ -0,0 +1,6 @@
+let tool = 'gun';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen'
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
