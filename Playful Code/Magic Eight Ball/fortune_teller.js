5f80d35ff48634e8ee5d8ebfe1638c58076e632e
popcorn150 <hungrymanchannel@gmail.com>
2024-02-14 17:15:21 +0100

    Working on a fortune telling ball, how cool is that! :)

---
+++ Playful Code/Magic Eight Ball/fortune_teller.js
@@ -0,0 +1,12 @@
+let userName = ''
+const userQuestion = 'How are you ?'
+let randomNumber = Math.floor(Math.random() * 8)
+let eightBall = ''
+
+if (userName = 'Jane') {
+    console.log(`Hello ${userName}!`)
+} else {
+    console.log('Hello!')
+}
+
+console.log(`${userName} asked this question: ${userQuestion}`)
