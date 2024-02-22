3a8be06f546cc262451653dcf11fa57316810ee7
popcorn150 <hungrymanchannel@gmail.com>
2024-02-22 22:17:18 +0100

    done with Gas Meter phase 1

--- Playful Code/GasMeter/gasMeter.js
+++ Playful Code/GasMeter/gasMeter.js
@@ -1,23 +1,25 @@
-let gas_rate = Math.floor(Math.random() * 100)
-// console.log(gas_rate)
+function check_gas_rating() {
+    const ratingExplanation = [
+        "This means that you can still use your gas for the time being without fear of it finishing !",
+        "This means that you can still use your gas but it could finish anytime soon.",
+        "Alert! No Gas, head to the nearest filling station for a refill!"
+    ]
 
-
-// switch (gas_rate) {
-//     case gas_rate >= 51:
-//         console.log(`Your gas is at ${gas_rate}% which means it doesn't need filling at the moment.`)
-//         break;
-//     case gas_rate <= 50:
-//         console.log(`Your gas is at ${gas_rate}% which means it does need filling as soon as possible!`)
-//         break;
-//     default:
-//         console.log('Error reading Gas!')
-//         break;
-// }
-
-if (gas_rate >= 51) {
-    console.log(`Your gas is at ${gas_rate}% which means it doesn't need filling at the moment.`)
-} else if (gas_rate <= 50) {
-    console.log(`Your gas is at ${gas_rate}% which means it does need filling as soon as possible!`)
-} else {
-    console.log('Error reading Gas!')
+    let gasRate = Math.floor(Math.random() * 101)
+    let img = document.getElementById('gas_images')
+    if (gasRate >= 50) {
+        document.getElementById('gas_readings').innerHTML = `Your Gas is at ${gasRate}%`
+        img.src = 'pictures/full_gas.png'
+        document.getElementById('gas_rating_explanation').innerHTML = ratingExplanation[0]
+    } else if (gasRate >= 20) {
+        document.getElementById('gas_readings').innerHTML = `Your Gas is at ${gasRate}%`,
+            img.src = 'pictures/normal_gas.png',
+            document.getElementById('gas_rating_explanation').innerHTML = ratingExplanation[1]
+    } else if (gasRate <= 19) {
+        document.getElementById('gas_readings').innerHTML = `Your Gas is at ${gasRate}%`,
+            img.src = 'pictures/empty_gas.png',
+            document.getElementById('gas_rating_explanation').innerHTML = ratingExplanation[2]
+    } else {
+        alert("Can not read Gas rating!!")
+    }
 }
