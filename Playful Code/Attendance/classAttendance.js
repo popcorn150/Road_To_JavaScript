02e26b612435a15dc085b2d6f408d47de1407e45
popcorn150 <hungrymanchannel@gmail.com>
2024-02-26 05:41:04 +0100

    added javascript to  the attendance project

--- Playful Code/Attendance/classAttendance.js
+++ Playful Code/Attendance/classAttendance.js
@@ -1,16 +1,45 @@
+const container = document.getElementsByClassName('attendanceContainer')[0]
 const userInput = document.getElementById('textInput')
 const clearList = document.getElementById('clearList')
 
-// clearList.addEventListener('click', function () {
-//     attendanceList.innerHTML = ''
-// })
 
-userInput.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        addAttendanceList()
+userInput.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        addToAttendance()
     }
 })
 
-function addAttendanceList() {
-    
+function addToAttendance() {
+    let divParent = document.createElement('div')
+    let divChild = document.createElement('div')
+    let checkIcon = document.createElement('i')
+    let notCheckIcon = document.createElement('i')
+
+    divParent.className = 'item'
+    divParent.innerHTML = '<div>' + userInput.value + '</div>'
+
+    checkIcon.className = 'fa fa-check-square'
+    checkIcon.style.color = 'lightgray'
+    checkIcon.addEventListener('click', function () {
+        checkIcon.style.color = 'limegreen'
+    })
+
+    divChild.appendChild(checkIcon)
+
+    notCheckIcon.className = 'fa fa-window-close'
+    notCheckIcon.style.color = 'lightgray'
+    notCheckIcon.addEventListener('click', function () {
+        notCheckIcon.style.color = 'red'
+    })
+
+    divChild.appendChild(notCheckIcon)
+
+    clearList.addEventListener('click', function () {
+        divParent.remove()
+    })
+
+    divParent.appendChild(divChild)
+
+    container.appendChild(divParent)
+    userInput.value = ''
 }
