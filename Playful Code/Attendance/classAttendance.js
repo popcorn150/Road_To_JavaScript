0e95996a76d71d610bc1233536b9ccd5d6a88488
popcorn150 <hungrymanchannel@gmail.com>
2024-02-26 03:38:37 +0100

    working on an attendance

--- Playful Code/Attendance/classAttendance.js
+++ Playful Code/Attendance/classAttendance.js
@@ -1,3 +1,21 @@
-function submitInput() {
-    console.log('Button clicked!')
+const userInput = document.getElementById('textInput')
+const attendanceList = document.getElementById('studentList')
+const clearList = document.getElementById('clearList')
+
+clearList.addEventListener('click', function () {
+    attendanceList.innerHTML = ''
+})
+
+userInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        addAttendanceList()
+    }
+})
+
+function addAttendanceList() {
+    let h2 = document.createElement('h2')
+    h2.innerHTML = '>'+ ' ' + userInput.value
+
+    attendanceList.insertAdjacentElement('beforeend', h2)
+    userInput.value = ''
 }
