d04467a6fc8afc6537a74e9a9e24539651fa2328
popcorn150 <hungrymanchannel@gmail.com>
2024-03-05 02:18:06 +0100

    done with the 8 magic ball

--- Playful Code/Magic Eight Ball/fortune_teller.js
+++ Playful Code/Magic Eight Ball/fortune_teller.js
@@ -1,5 +1,5 @@
 let userName = ''
-const userQuestion = 'How are you ?'
+const userQuestion = 'Can you talk?'
 let randomNumber = Math.floor(Math.random() * 8)
 let eightBall = ''
 
@@ -10,3 +10,34 @@ if (userName = 'Jane') {
 }
 
 console.log(`${userName} asked this question: ${userQuestion}`)
+
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain'
+        break;
+    case 1:
+        eightBall = 'It is decidedly so'
+        break
+    case 2:
+        eightBall = 'Reply hazy try again'
+        break
+    case 3:
+        eightBall = 'Cannot predict now'
+        break
+    case 4:
+        eightBall = 'Do not count on it'
+        break
+    case 5:
+        eightBall = 'My sources say no'
+        break
+    case 6:
+        eightBall = 'Outlook not so good'
+        break
+    case 7:
+        eightBall = 'Signs point to yes'
+        break
+    default:
+        break;
+}
+
+console.log(eightBall)
