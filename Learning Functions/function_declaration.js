b7d196732e0afdc00cacd3e92ebc8feb1e9c6d73
popcorn150 <hungrymanchannel@gmail.com>
2024-03-05 19:51:31 +0100

    Playing around with functions

---
+++ Learning Functions/function_declaration.js
@@ -0,0 +1,14 @@
+function getReminder() {
+    console.log('Water the plants.')
+}
+
+function greetInSpanish() {
+    console.log('Buenas tardes.')
+}
+
+function sayThanks(customerName) {
+    console.log(`Thank you for your purchase ${customerName}! We appreciate your business.`)
+}
+sayThanks('David')
+sayThanks('Mary')
+sayThanks('John')
