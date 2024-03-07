2bcc57bcae266d3f95100cbcdcc9812b71220faa
popcorn150 <hungrymanchannel@gmail.com>
2024-03-08 00:06:43 +0100

    Learning javascript scopes

---
+++ Learning Scope/blocks_scope.js
@@ -0,0 +1,17 @@
+const city = 'New York City'
+
+function logCitySkyline(){
+    let skyscraper = 'Empire State Building'
+    return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+console.log(logCitySkyline())
+
+// Global scope
+const satellite = 'The Moon'
+const galaxy = 'The Milky Way'
+const stars = 'North Star'
+
+function callMyNightSky(){
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky())
