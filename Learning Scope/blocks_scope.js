ce87bdad1a016495502f512b499fcd14f18086d0
popcorn150 <hungrymanchannel@gmail.com>
2024-03-08 01:47:07 +0100

    Finally done with js scoping

--- Learning Scope/blocks_scope.js
+++ Learning Scope/blocks_scope.js
@@ -1,6 +1,6 @@
 const city = 'New York City'
 
-function logCitySkyline(){
+function logCitySkyline() {
     let skyscraper = 'Empire State Building'
     return 'The stars over the ' + skyscraper + ' in ' + city;
 }
@@ -11,15 +11,29 @@ const satellite = 'The Moon'
 const galaxy = 'The Milky Way'
 const stars = 'North Star'
 
-function callMyNightSky(){
+function callMyNightSky() {
+    let stars = 'Sirius' // Scope pollution (overrides the previous varaiable's value)
     return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
 }
 console.log(callMyNightSky())
+console.log(stars) // Calling the new variable's value
 
 // Block scope
-function logVisibleLightWaves(){
+function logVisibleLightWaves() {
     const lightWaves = 'Moonlight'
     console.log(lightWaves)
 }
 logVisibleLightWaves()
-console.log(lightWaves)
+
+// Practice good scoping
+const logVisibleLightWavesII = () => {
+    let lightWaves = 'Moonlight';
+    let region = 'The Arctic';
+    if (region === 'The Arctic') {
+        let lightWaves = 'Northern Lights'
+        console.log(lightWaves)
+    }
+    console.log(lightWaves);
+};
+
+logVisibleLightWavesII();
