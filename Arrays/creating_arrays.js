3b955f887f20f2fa7308d9acb93dd1368e9190ed
popcorn150 <hungrymanchannel@gmail.com>
2024-03-10 23:52:32 +0100

    Working on arrays

---
+++ Arrays/creating_arrays.js
@@ -0,0 +1,26 @@
+const hobbies = ['watching anime', 'gaming', 'cooking']
+console.log(hobbies)
+
+// Accessing array elements
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.']
+let listItem = famousSayings[0]
+console.log(famousSayings[2])
+
+// Update Elements
+let groceryList = ['bread', 'tomatoes', 'milk']
+groceryList[1] = 'avocados'
+console.log(groceryList)
+
+// Arrays with let and const
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha']
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork']
+
+condiments[0] = 'Mayo'
+console.log(condiments)
+condiments = ['Mayo']
+console.log(condiments)
+utensils[3] = 'Spoon'
+console.log(utensils)
+
+// The .length property
