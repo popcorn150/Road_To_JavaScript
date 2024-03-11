3eb60f4a0e2f693c3734a99ce25a9ffc6022b0a7
popcorn150 <hungrymanchannel@gmail.com>
2024-03-11 19:56:03 +0100

    rounding off with js array

---
+++ Arrays/nestedArrays.js
@@ -0,0 +1,4 @@
+let emotionClusters = [['Happy'], ['Sad', 'Depressed', 'Lonely'], ['Anxiety', 'Fear']]
+const targetEmotion = emotionClusters[2][0]
+let question = 'What emotion do you feel currently? : '
+console.log(`${question} '${targetEmotion}'`)
