155685701a9ff273deb4ff642a9385602a216fab
popcorn150 <hungrymanchannel@gmail.com>
2024-04-01 21:36:44 +0100

    played around with js functions

---
+++ Js Functions/functions.js
@@ -0,0 +1,26 @@
+/* Functions as Data */
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+    for (let i = 1; i <= 1000000; i++) {
+        if ((2 + 2) != 4) {
+            console.log('Something has gone very wrong :( ');
+        }
+    }
+};
+const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes
+isTwoPlusTwo()
+console.log(isTwoPlusTwo.name)
+
+/* Functions as Parameters */
+const addTwo = num => {
+    return num + 2;
+}
+
+const checkConsistentOutput = (func, val) => {
+    const checkA = val + 2
+    const checkB = func(val)
+    if (checkA === checkB) {
+        return func(val)
+    }
+    console.log('inconsistent results');
+}
+console.log(checkConsistentOutput(addTwo, 15))
