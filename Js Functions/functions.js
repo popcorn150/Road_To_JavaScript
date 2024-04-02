f5e9817fd81089970a0bd1a769471bd897b20d7b
popcorn150 <hungrymanchannel@gmail.com>
2024-04-02 22:36:20 +0100

    still played around with loops and functions

--- Js Functions/functions.js
+++ Js Functions/functions.js
@@ -1,26 +1,42 @@
-/* Functions as Data */
-const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
-    for (let i = 1; i <= 1000000; i++) {
-        if ((2 + 2) != 4) {
-            console.log('Something has gone very wrong :( ');
-        }
-    }
-};
-const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes
-isTwoPlusTwo()
-console.log(isTwoPlusTwo.name)
+// /* Functions as Data */
+// const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+//     for (let i = 1; i <= 1000000; i++) {
+//         if ((2 + 2) != 4) {
+//             console.log('Something has gone very wrong :( ');
+//         }
+//     }
+// };
+// const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes
+// isTwoPlusTwo()
+// console.log(isTwoPlusTwo.name)
 
-/* Functions as Parameters */
-const addTwo = num => {
-    return num + 2;
+// /* Functions as Parameters */
+// const addTwo = num => {
+//     return num + 2;
+// }
+
+// const checkConsistentOutput = (func, val) => {
+//     const checkA = val + 2
+//     const checkB = func(val)
+//     if (checkA === checkB) {
+//         return func(val)
+//     }
+//     console.log('inconsistent results');
+// }
+// console.log(checkConsistentOutput(addTwo, 15))
+
+const hungry = () => {
+    return 'I want to eat!!'
 }
 
-const checkConsistentOutput = (func, val) => {
-    const checkA = val + 2
-    const checkB = func(val)
-    if (checkA === checkB) {
-        return func(val)
-    }
-    console.log('inconsistent results');
+const notHungry = () => {
+    return 'I am not hungry!!'
 }
-console.log(checkConsistentOutput(addTwo, 15))
+
+for (let i = 0; i < 10; i++) {
+    if (i < 10) {
+        console.log(hungry())
+    } else {
+        console.log(notHungry())
+    }
+}
