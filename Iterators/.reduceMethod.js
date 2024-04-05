94b08846cb6e483d1fa05a72f16f24f1a95faee0
popcorn150 <hungrymanchannel@gmail.com>
2024-04-06 00:32:39 +0100

    msg

--- Iterators/.reduceMethod.js
+++ Iterators/.reduceMethod.js
@@ -8,3 +8,20 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
 }, 10)
 
 console.log(newSum)
+
+/* Revision */
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(word => {
+    return word.length < 6;
+}));
+
+// Use filter to create a new array
+const interestingWords = words.filter((word) => { return word.length > 5 });
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every((word) => { return word.length > 5 }));
