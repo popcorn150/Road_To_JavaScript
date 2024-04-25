6c719e798a520311175c88b1158c029646877375
popcorn150 <hungrymanchannel@gmail.com>
2024-04-25 22:59:14 +0100

    object!

--- JavaScript => React/Variables and Data Types/index.js
+++ JavaScript => React/Variables and Data Types/index.js
@@ -1,22 +1,22 @@
 /**Person Function */
-function person(name, age){
+function person(name, age) {
     let introduction = `My name is ${name} and I am ${age} years old.`
     console.log(introduction)
 }
 person('David', 20)
 
 /**isEven Function */
-function isEven(num){
+function isEven(num) {
     if (num % 2 === 0) {
         console.log(`${num} is an Even number`)
-    }else{
+    } else {
         console.log(`${num} is not an Even number`)
     }
 }
 isEven(83)
 
 /**printFruits Function */
-function printFruits(fruits){
+function printFruits(fruits) {
     for (let i = 0; i < fruits.length; i++) {
         console.log(fruits[i])
     }
@@ -24,13 +24,13 @@ function printFruits(fruits){
 printFruits(['Apple', 'Banana', 'Orange'])
 
 /**reverseString Function 1*/
-function reverseString(str){
+function reverseString(str) {
     console.log(str.split('').reverse().join(''))
 }
 reverseString('David')
 
 /**reverseString Function 2*/
-function reverseString(str){
+function reverseString(str) {
     let newStr = ''
     for (let i = str.length - 1; i >= 0; i--) {
         newStr += str[i];
@@ -38,3 +38,20 @@ function reverseString(str){
     console.log(newStr)
 }
 reverseString('Hello')
+
+/**getUserInfo Function*/
+// let user = {
+//     name: 'David',
+//     age: 19,
+//     email: 'hungrymanchannel@gmail.com'
+// }
+function getUserInfo(user) {
+    console.log(
+        `Name: ${user.name}\n Age; ${user.age}\n Email: ${user.email}`
+    )
+}
+getUserInfo({
+    name: 'David',
+    age: 19,
+    email: 'hungrymanchannel@gmail.com'
+})
