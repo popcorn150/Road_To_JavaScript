5c99dc5d40d68ebd24d6495da0cfe9a9e9b5df88
popcorn150 <hungrymanchannel@gmail.com>
2024-05-01 13:53:11 +0100

    done with js object tests

--- JavaScript => React/Arrays & Objects/index.js
+++ JavaScript => React/Arrays & Objects/index.js
@@ -4,7 +4,7 @@
 function reverseArray(arr) {
     let reversedArray = []
     return arr.slice().reverse()
-    
+
 }
 let array = ['Apple', 'Banana', 3, 7, 2]
 console.log(reverseArray(array))
@@ -18,7 +18,7 @@ console.log(findLargest(numbers))
 
 //removeDuplicates Function
 function removeDuplicates(arr) {
-   return arr.filter((item, index) => arr.indexOf(item) === index)
+    return arr.filter((item, index) => arr.indexOf(item) === index)
 }
 let duplicateArray = ['Man', 'Boy', 'Girl', 'Woman', 'apple', 'apple', 'Man', 'Man', 'Boy']
 console.log(removeDuplicates(duplicateArray))
@@ -31,7 +31,29 @@ let person = {
     age: 28,
     gender: 'Male'
 }
-function printPersonInfo(personInfo){
+function printPersonInfo(personInfo) {
     return `Name: ${personInfo.name}, Age: ${personInfo.age}, Gender: ${personInfo.gender}`
 }
 console.log(printPersonInfo(person))
+
+//Count properties Object
+function countProperties(obj) {
+    return Object.keys(obj).length
+}
+const personObj = {
+    firstName: 'Daniel',
+    age: 40,
+    gender: 'Male',
+    color: 'Green',
+    favoriteFood: 'Mashemello Brownine Cake'
+}
+console.log(countProperties(personObj))
+
+//Merge Object
+function mergeObjects(obj1, obj2){
+    return {...obj1, ...obj2}
+}
+const obj1 = {a:1, b:2, c:3, d:4}
+const obj2 = {a:5, f:6, g:7, c:10}
+
+console.log(mergeObjects(obj1, obj2))
